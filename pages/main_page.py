82d95a170c616c50c0226ae9dd78fccefe1e20ad
vnakonechnuy <vnakonechnuy@i.ua>
2021-11-09 22:49:49 +0200

    Добавлена папка с файлами PageObject

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+class BasePage:
+
+    def __init__(self, url, browser):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
