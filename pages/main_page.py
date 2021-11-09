8fe6897a74f71afa7f3fd77432a2aafd37ac6ae5
vnakonechnuy <vnakonechnuy@i.ua>
2021-11-09 22:51:51 +0200

    Исправлены файлы PageObject

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,3 @@
-from selenium import webdriver
 
 
-class BasePage:
 
-    def __init__(self, url, browser):
-        self.browser = browser
-        self.url = url
-
-    def open(self):
-        self.browser.get(self.url)
