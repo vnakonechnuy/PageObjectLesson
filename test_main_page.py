d024c1e947cd1fb8cbcbdc97a0bf0d7cf20bc0a9
vnakonechnuy <vnakonechnuy@i.ua>
2021-11-13 10:42:26 +0200

    Add tests and pageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,14 @@
+from .pages.main_page import MainPage
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer '
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link = browser.find_element_by_css_selector("#registration_link")
     login_link.click()
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
